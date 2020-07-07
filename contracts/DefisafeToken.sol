a949ee2b85e9c3a13208aa083ab5f59e5fc729b5
木生 <455194872@qq.com>
2020-07-07 11:39:31 +0800

    compiler error(#2) *  Refactor the code *  Solve the compilation error problem

--- contracts/DefisafeToken.sol
+++ contracts/DefisafeToken.sol
@@ -4,7 +4,7 @@ import './ERC20.sol';
 import './SafeMath.sol';
 import './RoleManageContract.sol';
 
-contract StandardToken is ERC20,Burnable{
+contract DeFiSafeToken is ERC20,Burnable{
 
     using SafeMath for uint256;
     mapping (address => uint256) private _balances;
@@ -14,100 +14,13 @@ contract StandardToken is ERC20,Burnable{
 
     string  public  constant _name = "DefiSafe";
     string  public  constant _symbol = "DSE";
-    uint    public  constant _decimals = 18; 
+    uint256 public  constant _decimals = 18; 
 
-     /**
-     * @dev Returns the number of decimals used to get its user representation.
-     * For example, if `decimals` equals `2`, a balance of `505` tokens should
-     * be displayed to a user as `5,05` (`505 / 10 ** 2`).
-     *
-     * Tokens usually opt for a value of 18, imitating the relationship between
-     * Ether and Wei. This is the value {ERC20} uses, unless {_setupDecimals} is
-     * called.
-     *
-     * NOTE: This information is only used for _display_ purposes: it in
-     * no way affects any of the arithmetic of the contract, including
-     * {IERC20-balanceOf} and {IERC20-transfer}.
-     */
-    function decimals() public view returns (uint8) {
-        return _decimals;
-    }
-
-    /**
-     * @dev See {ERC20-totalSupply}.
-     */
-    function totalSupply() public view override returns (uint256) {
-        return _totalSupply;
-    }
-
-    /**
-     * @dev Returns the name of the token.
-     */
-    function name() public view returns (string memory) {
-        return _name;
-    }
-
-    /**
-     * @dev Returns the symbol of the token, usually a shorter version of the
-     * name.
-     */
-    function symbol() public view returns (string memory) {
-        return _symbol;
-    }
-
-
-
-  function transfer(address _to, uint256 _value) public override returns (bool) {
-    balances[msg.sender] = balances[msg.sender].sub(_value);
-    balances[_to] = balances[_to].add(_value);
-    emit Transfer(msg.sender, _to, _value);
-    return true;
-  }
-
-    /**
-     * @dev See {ERC20-balanceOf}.
-     */
-    function balanceOf(address account) public view override returns (uint256) {
-        return _balances[account];
-    }
-
-  function transferFrom(address _from, address _to, uint256 _value) public virtual override returns (bool) {
-    uint256 _allowance = allowed[_from][msg.sender];
-
-    // Check is not needed because sub(_allowance, _value) will already throw if this condition is not met
-    // require (_value <= _allowance);
-
-    // KYBER-NOTE! code changed to comply with ERC20 standard
-    balances[_from] = balances[_from].sub(_value);
-    balances[_to] = balances[_to].add(_value);
-    //balances[_from] = balances[_from].sub(_value); // this was removed
-    allowed[_from][msg.sender] = _allowance.sub(_value);
-    emit Transfer(_from, _to, _value);
-    return true;
-  }
-
-  function approve(address _spender, uint256 _value) public virtual override returns (bool) {
-
-    require((_value == 0) || (allowed[msg.sender][_spender] == 0));
-
-    allowed[msg.sender][_spender] = _value;
-
-    emit Approval(msg.sender, _spender, _value);
-    return true;
-  }
-
-  function allowance(address _owner, address _spender) public view virtual override returns (uint256 remaining) {
-    return allowed[_owner][_spender];
-  }
-}
-
-
-contract DeFiSafeToken is StandardToken, Burnable{
 
     constructor(uint256 _tokenTotalAmount) public {
         _balances[msg.sender] = _tokenTotalAmount;
         _totalSupply = _tokenTotalAmount;
-        Transfer(address(0x0), msg.sender, tokenTotalAmount);
+        emit Transfer(address(0x0), msg.sender, _tokenTotalAmount);
 
         transferOwnership(msg.sender); // admin could drain tokens that were sent here by mistake
     }
@@ -144,14 +57,96 @@ contract DeFiSafeToken is StandardToken, Burnable{
      * Emits a {Transfer} event with `to` set to the zero address.
      *
      */
-    function burn(uint _value) onlyBurner
+    function burn(uint _value) public onlyBurner
         returns (bool){
-        balances[msg.sender] = balances[msg.sender].sub(_value);
-        totalSupply = totalSupply.sub(_value);
+        _balances[msg.sender] = _balances[msg.sender].sub(_value);
+        _totalSupply = _totalSupply.sub(_value);
         Burn(msg.sender, _value);
         Transfer(msg.sender, address(0x0), _value);
         return true;
     }
 
+     /**
+     * @dev Returns the number of decimals used to get its user representation.
+     * For example, if `decimals` equals `2`, a balance of `505` tokens should
+     * be displayed to a user as `5,05` (`505 / 10 ** 2`).
+     *
+     * Tokens usually opt for a value of 18, imitating the relationship between
+     * Ether and Wei. This is the value {ERC20} uses, unless {_setupDecimals} is
+     * called.
+     *
+     * NOTE: This information is only used for _display_ purposes: it in
+     * no way affects any of the arithmetic of the contract, including
+     * {IERC20-balanceOf} and {IERC20-transfer}.
+     */
+    function decimals() public view returns (uint256) {
+        return _decimals;
+    }
+
+    /**
+     * @dev See {ERC20-totalSupply}.
+     */
+    function totalSupply() public view override returns (uint256) {
+        return _totalSupply;
+    }
+
+    /**
+     * @dev Returns the name of the token.
+     */
+    function name() public view returns (string memory) {
+        return _name;
+    }
+
+    /**
+     * @dev Returns the symbol of the token, usually a shorter version of the
+     * name.
+     */
+    function symbol() public view returns (string memory) {
+        return _symbol;
+    }
+
+
+
+  function transfer(address _to, uint256 _value) public override returns (bool) {
+    _balances[msg.sender] = _balances[msg.sender].sub(_value);
+    _balances[_to] = _balances[_to].add(_value);
+    emit Transfer(msg.sender, _to, _value);
+    return true;
+  }
+
+    /**
+     * @dev See {ERC20-balanceOf}.
+     */
+    function balanceOf(address account) public view override returns (uint256) {
+        return _balances[account];
+    }
+
+  function transferFrom(address _from, address _to, uint256 _value) public virtual override returns (bool) {
+    uint256 allowance = _allowances[_from][msg.sender];
+
+    // Check is not needed because sub(_allowance, _value) will already throw if this condition is not met
+    // require (_value <= _allowance);
+
+    // KYBER-NOTE! code changed to comply with ERC20 standard
+    _balances[_from] = _balances[_from].sub(_value);
+    _balances[_to] = _balances[_to].add(_value);
+    //balances[_from] = balances[_from].sub(_value); // this was removed
+    _allowances[_from][msg.sender] = allowance.sub(_value);
+    emit Transfer(_from, _to, _value);
+    return true;
+  }
+
+  function approve(address _spender, uint256 _value) public virtual override returns (bool) {
+
+    require((_value == 0) || (_allowances[msg.sender][_spender] == 0));
+
+    _allowances[msg.sender][_spender] = _value;
+
+    emit Approval(msg.sender, _spender, _value);
+    return true;
+  }
+
+  function allowance(address _owner, address _spender) public view virtual override returns (uint256 remaining) {
+    return _allowances[_owner][_spender];
+  }
 }
-*/
--- contracts/SafeMath.sol
+++ contracts/SafeMath.sol
@@ -1,3 +1,5 @@
+pragma solidity ^0.6.0;
+
 
 library SafeMath {
 
